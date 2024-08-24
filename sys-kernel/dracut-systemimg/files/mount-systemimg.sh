c92a72c25e9fe3b7e7d46a31454551d96b69e58c
Tomoatsu Shimada <shimada@walbrix.com>
2024-08-25 06:59:05 +0900

    consider read-only boot partition(like cdrom)

--- sys-kernel/dracut-systemimg/files/mount-systemimg.sh
+++ sys-kernel/dracut-systemimg/files/mount-systemimg.sh
@@ -12,12 +12,17 @@ SYSTEMIMG_BOOT_PARTITION="$(label_uuid_to_dev UUID=$UUID)"
 SYSTEMIMG_DATA_PARTITION="$(label_uuid_to_dev LABEL=data-$UUID)"
 
 [ -e /run/initramfs/boot ] || mkdir -m 0755 -p /run/initramfs/boot
-/usr/bin/fsck.fat -aw "$SYSTEMIMG_BOOT_PARTITION"
-mount -t vfat "$SYSTEMIMG_BOOT_PARTITION" /run/initramfs/boot || die "Failed to mount boot partition"
-/usr/bin/date > /run/initramfs/boot/boottime.txt || info "creating boottime.txt failed"
+eval $(blkid  -o udev -s TYPE "$SYSTEMIMG_BOOT_PARTITION")
+if [ "$ID_FS_TYPE" == "vfat" ]; then
+       /usr/bin/fsck.fat -aw "$SYSTEMIMG_BOOT_PARTITION"
+       mount -t vfat "$SYSTEMIMG_BOOT_PARTITION" /run/initramfs/boot || die "Failed to mount boot partition(FAT)"
+       /usr/bin/date > /run/initramfs/boot/boottime.txt || info "creating boottime.txt failed"
+else
+       mount -t auto -o ro "$SYSTEMIMG_BOOT_PARTITION" /run/initramfs/boot || die "Failed to mount boot partition(readonly)"
+fi
 
 [ -e /run/initramfs/ro ] || mkdir -m 0755 -p /run/initramfs/ro
-mount -o loop,ro -t squashfs /run/initramfs/boot/system.img /run/initramfs/ro || die "Failed to mount system.img"
+mount -o loop,ro -t squashfs /run/initramfs/boot/system.img /run/initramfs/ro || die "Failed to loopback mount system.img"
 
 [ -e /run/initramfs/rw ] || mkdir -m 0755 -p /run/initramfs/rw
 getargbool 0 systemimg.transient || mount -t auto "$SYSTEMIMG_DATA_PARTITION" /run/initramfs/rw
