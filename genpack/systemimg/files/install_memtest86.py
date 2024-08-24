c6d06ae2ad9dd6d76b060bd7720e31571b5130b2
Tomoatsu Shimada <shimada@walbrix.com>
2024-08-25 07:14:01 +0900

    install_memtest86 fixed

--- genpack/systemimg/files/install_memtest86.py
+++ genpack/systemimg/files/install_memtest86.py
@@ -1,11 +1,11 @@
 import os,shutil,logging
 
-def configure(root):
-    efi_path = os.path.join(root, "run/initramfs/boot/efi/boot/memtest86.efi")
+def configure(ini):
+    efi_path = "/run/initramfs/boot/efi/boot/memtest86.efi"
     if os.path.exists(efi_path): 
         logging.debug("memtest86 EFI binary already installed")
         return
-    boot_path = os.path.join(root, "boot/memtest86-bin.efi")
+    boot_path = "/boot/memtest86-bin.efi"
     if not os.path.isfile(boot_path): return
     os.makedirs(os.path.dirname(efi_path), exist_ok=True)
     shutil.copyfile(boot_path, efi_path)
