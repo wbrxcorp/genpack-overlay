c26a0689d6d1e230cfcda5a2cf183beebabad4fd
Tomoatsu Shimada <shimada@walbrix.com>
2024-08-31 17:12:03 +0900

    vm volume can fallback to regular directory

--- app-admin/wb/files/default_volume.py
+++ app-admin/wb/files/default_volume.py
@@ -1,10 +1,23 @@
 import os,subprocess,logging
 
+def is_btrfs_mounted(mountpoint):
+    with open("/proc/mounts") as f:
+        for line in f:
+            splitted = line.split()
+            if line.split()[1] == mountpoint and line.split()[2] == "btrfs":
+                return True
+    return False
+
 def configure(ini):
-    subvol = "/run/initramfs/rw/vm"
+    rw_layer = "/run/initramfs/rw"
+    subvol = os.path.join(rw_layer, "vm")
     if not os.path.exists(subvol):
-        if subprocess.call(["/sbin/btrfs","subvolume","create",subvol]) != 0:
-            logging.warning("Failed to create vm data subvolume under data partition.")
+        if is_btrfs_mounted(rw_layer):
+            if subprocess.call(["/sbin/btrfs","subvolume","create",subvol]) != 0:
+                logging.warning("Failed to create vm data subvolume under data partition.")
+        else:
+            logging.warning("Data partition is not mounted as btrfs. Falling back to regular directory")
+            os.makedirs(subvol, exist_ok=True)
 
     if os.path.isdir(subvol):
         var_vm_default = "/var/vm/@default"
@@ -14,3 +27,6 @@ def configure(ini):
             return
     #else
     logging.warning("Default VM subvolume not mounted.")
+
+if __file__ == "__main__":
+    configure(None)
