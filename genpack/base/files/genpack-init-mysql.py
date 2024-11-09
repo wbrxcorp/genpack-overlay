2ed5428482c322aeaa076869a0b10b1e3fc90c9a
Tomoatsu Shimada <shimada@walbrix.com>
2024-11-09 21:54:39 +0900

    do not copy mysql data directory if capacity is not enough

--- genpack/base/files/genpack-init-mysql.py
+++ genpack/base/files/genpack-init-mysql.py
@@ -6,8 +6,20 @@ def configure():
         logging.warning("No MySQL data directory")
         return
     #else
+
+    if not os.path.exists("/run/initramfs/rw"):
+        logging.error("No /run/initramfs/rw") # impossible, though
+        return
+
     mysql_work = "/run/initramfs/rw/mysql"
     if not os.path.exists(mysql_work):
+        # check if available capacity of /run/initramfs/rw is enough
+        statvfs = os.statvfs("/run/initramfs/rw")
+        if statvfs.f_bavail * statvfs.f_frsize < 1024 * 1024 * 768: # 768MB
+            logging.error("Insufficient space in /run/initramfs/rw (initramfs?)")
+            return
+        #else
+
         if subprocess.call(["/bin/cp", "-a", mysql_orig, mysql_work]) == 0: # somehow shutil.copytree doesn't preserve file owner
             logging.info("MySQL data directory copied.")
         else:
