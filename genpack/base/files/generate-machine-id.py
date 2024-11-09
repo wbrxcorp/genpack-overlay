dc377ee210c465a140270c8d2f7d4e60b19e5e90
Tomoatsu Shimada <shimada@walbrix.com>
2024-11-09 22:42:37 +0900

    use logging instead of print

--- genpack/base/files/generate-machine-id.py
+++ genpack/base/files/generate-machine-id.py
@@ -1,4 +1,4 @@
-import os,subprocess
+import os,subprocess,logging
 
 MACHINE_ID = "/etc/machine-id"
 
@@ -6,4 +6,4 @@ def configure():
     if os.path.exists(MACHINE_ID): return
     #else
     subprocess.check_call(["systemd-machine-id-setup"])
-    print("Machine ID generated.")
+    logging.info("Machine ID generated.")
