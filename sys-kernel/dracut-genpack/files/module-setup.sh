d68b4e488f95b40cbb24c559301fab3e0a80f3d7
Tomoatsu Shimada <shimada@walbrix.com>
2025-01-17 10:59:51 +0900

    /usr/bin/touch should explicitly be included

--- sys-kernel/dracut-genpack/files/module-setup.sh
+++ sys-kernel/dracut-genpack/files/module-setup.sh
@@ -16,6 +16,7 @@ installkernel() {
 install() {
     inst_binary /usr/bin/date
     inst_binary /usr/bin/fsck.fat
+    inst_binary /usr/bin/touch
     inst_hook cmdline 30 "$moddir/check-systemimg-root.sh"
     inst_hook mount 01 "$moddir/mount-genpack.sh"     # overlay on top of block device
 }
