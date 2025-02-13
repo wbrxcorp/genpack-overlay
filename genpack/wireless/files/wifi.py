c8cbbe9fdee56faf22af0bea9076b0e4daaa3fd3
Tomoatsu Shimada <shimada@walbrix.com>
2025-02-14 02:33:18 +0900

    WPA3 support added

--- genpack/wireless/files/wifi.py
+++ genpack/wireless/files/wifi.py
@@ -1,10 +1,13 @@
-import os,subprocess,glob,logging,time,builtins
+import logging,time
+
+from genpack_init import root_path,coldplug,enable_systemd_service
 
 def get_wireless_interface_name(wait):
     for i in range(0, wait):
-        for interface in glob.glob("/sys/class/net/wl*"):
-            if os.path.isdir(os.path.join(interface, "wireless")):
-                return os.path.basename(interface)
+        for interface in root_path("/sys/class/net").glob("wl*"):
+            logging.debug("Looking for directory wireless in %s" % interface)
+            if interface.joinpath("wireless").is_dir():
+                return interface.name
         logging.debug("Waiting for WiFi interface to be activated...")
         time.sleep(1)
     return None
@@ -15,14 +18,18 @@ def configure(ini):
         logging.debug("Leaving WiFi configuration.")
         return
     #else
-    for conf in glob.glob("/etc/wpa_supplicant/wpa_supplicant-*.conf"):
-        os.unlink(conf)
+    logging.debug("WiFi SSID: %s" % wifi_ssid)
+    wpa_supplicant_dir = root_path("/etc/wpa_supplicant")
+    print(wpa_supplicant_dir)
+    wpa_supplicant_dir.mkdir(parents=True,exist_ok=True)
+    for conf in wpa_supplicant_dir.glob("wpa_supplicant-*.conf"):
+        conf.unlink()
         logging.debug("%s removed" % conf)
-    for link in glob.glob("/etc/systemd/system/multi-user.target.wants/wpa_supplicant@*.service"):
-        os.unlink(link)
+    for link in root_path("/etc/systemd/system/multi-user.target.wants").glob("wpa_supplicant@*.service"):
+        link.unlink()
         logging.debug("%s removed" % link)
 
-    hasattr(builtins, 'coldplug') and coldplug() # type: ignore
+    coldplug()
 
     interface = ini.get("_default", "wifi_interface", fallback=None)    
     if interface is None: interface = get_wireless_interface_name(ini.getint("_default", "wifi_wait", fallback=3))
@@ -34,17 +41,40 @@ def configure(ini):
     #else
     logging.info("Using wireless interface %s." % interface)
 
-    conf = "/etc/wpa_supplicant/wpa_supplicant-%s.conf" % interface
+    wpa3 = ini.getboolean("_default", "wifi_wpa3", fallback=False)
+    sae_pwe = ini.getint("_default", "wifi_sae_pwe", fallback=2 if wpa3 else None)
+
+    conf = wpa_supplicant_dir.joinpath("wpa_supplicant-%s.conf" % interface)
     with open(conf, "w") as f:
+        if sae_pwe is not None:
+            f.write("sae_pwe=%d\n" % sae_pwe)
         f.write('network={\n\tssid="%s"\n' % wifi_ssid)
         wifi_key = ini.get("_default", "wifi_key", fallback=None)
         if wifi_key is not None:
-            f.write('\tpsk="%s"\n' % wifi_key)
+            if wpa3:
+                f.write('\tkey_mgmt=SAE\n')
+                f.write('\tsae_password="%s"\n' % wifi_key)
+                f.write('\tieee80211w=2\n')
+            else:
+                f.write('\tpsk="%s"\n' % wifi_key)
         f.write('}')
         logging.debug("%s created" % conf)
 
-    subprocess.run(["systemctl", "enable", "wpa_supplicant@%s" % interface], check=True)
-    logging.info("Service wpa_supplicant@%s enabled." % interface)
+    enable_systemd_service("wpa_supplicant@%s" % interface)
 
+# PYTHONPATH=/path/to/mock python3 wifi.py
 if __name__ == "__main__":
-    pass
+    import argparse,configparser
+    logging.basicConfig(level=logging.DEBUG)
+
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("--wpa3", default=False, action="store_true")
+    args = argparser.parse_args()
+
+    ini = configparser.ConfigParser()
+    ini.add_section("_default")
+    ini.set("_default", "wifi_ssid", "my_wifi")
+    ini.set("_default", "wifi_key", "my_password")
+    if args.wpa3:
+        ini.set("_default", "wifi_wpa3", "yes")
+    configure(ini)
