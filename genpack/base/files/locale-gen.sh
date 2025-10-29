5096076c2cbc8c39c4c5468fefce73d517fadb8e
Tomoatsu Shimada <shimada@walbrix.com>
2025-10-29 21:05:40 +0900

    avoid generating locale archive every time

--- genpack/base/files/locale-gen.sh
+++ genpack/base/files/locale-gen.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 set -e
-sed -i 's/^#en_US ISO-8859-1/en_US ISO-8859-1/g' /etc/locale.gen
-sed -i 's/^#en_US.UTF-8/en_US.UTF-8/g' /etc/locale.gen
-sed -i 's/^#ja_JP.UTF-8/ja_JP.UTF-8/g' /etc/locale.gen
-locale-gen
+recursive-touch /usr/lib/locale/locale-archive
