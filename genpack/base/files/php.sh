c2d15966d84b2dd55ce16515611fd17175175274
Tomoatsu Shimada <shimada@walbrix.com>
2025-11-29 13:04:02 +0900

    do eselect php for apache and fpm

--- genpack/base/files/php.sh
+++ genpack/base/files/php.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 set -e
 eselect php set cli $(eselect php show cli)
+
+if [ -f /etc/apache2/modules.d/70_mod_php.conf ]; then
+    eselect php set apache2 $(eselect php show apache2)
+fi
+
+if [ -f /usr/libexec/php-fpm-launcher ]; then
+    eselect php set fpm $(eselect php show fpm)
+fi
