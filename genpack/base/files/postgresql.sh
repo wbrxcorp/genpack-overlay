22132c2fe78669fdbbcc6d6e307de75e2e9b55a0
Tomoatsu Shimada <shimada@walbrix.com>
2026-05-18 10:18:25 +0900

    use ln -sf

--- genpack/base/files/postgresql.sh
+++ genpack/base/files/postgresql.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 set -e
 PGSQL_VERSION=$(eselect postgresql show)
+
 emerge --config dev-db/postgresql # setup initial database
 eselect postgresql set $PGSQL_VERSION # create symlinks
 
-ln -s postgresql-$PGSQL_VERSION.service /usr/lib/systemd/system/postgresql.service
-ln -s postgresql-$PGSQL_VERSION /etc/postgresql
+echo "Creating symlink /usr/lib/systemd/system/postgresql.service -> postgresql-$PGSQL_VERSION.service"
+ln -sf postgresql-$PGSQL_VERSION.service /usr/lib/systemd/system/postgresql.service
+echo "Creating symlink /etc/postgresql -> postgresql-$PGSQL_VERSION"
+ln -sf postgresql-$PGSQL_VERSION /etc/postgresql
